7b49e1f6fad8ed294a7e5764d24adf184f0bd01e
Gyengus(Tm) <gyengus@tita.gyengus.hu>
2014-12-25 11:49:12 +0100

    Extend the example: - read a button - setting a pin to high state - setting a pin to low state - close used pins

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 rpi_gpio_test_nodejs
 ====================
+
+Raspberry Pi GPIO read/write with Node.js
+
+This example shows how to read and write Rapsberry Pi GPIO state with rpi-gpio.js
+
+<h3>How to run</h3>
+
+Clone this repository and go to the project directory
+<pre>
+$ npm install
+$ sudo node index.js
+</pre>
+Please note: You must run this in root account.
+
+Coming soon: a detailed article in my blog
--- index.js
+++ index.js
@@ -1,14 +1,44 @@
-var gpio = require("./rpi-gpio.js");
-var PIN = 22;
+var gpio = require('./rpi-gpio.js');
+var PIN = 22; // RED LED
+var BTN = 16; // Button
 
-gpio.setup(PIN, gpio.DIR_OUT, function() {
+function redledoff() {
     gpio.write(PIN, false, function(err) {
 	if (err) {
 	    console.log(err);
-	    process.exit();
 	} else {
-	    console.log("OK");
-	    process.exit(); 
+	    console.log('RED LED Off');
+	}
+    });
+} // redledoff
+
+// Read button state
+gpio.setup(BTN, gpio.DIR_IN, function() {
+    gpio.read(BTN, function(err, value) {
+	if (err) {
+	    console.log(err);
+	} else {
+	    console.log('Button state: ' + value);
 	}
     });
 });
+
+// Set RED LED on for 5 secs
+gpio.setup(PIN, gpio.DIR_OUT, function() {
+    gpio.write(PIN, true, function(err) {
+	if (err) {
+	    console.log(err);
+	} else {
+	    console.log('RED LED On');
+	    setTimeout(redledoff, 5000);
+	}
+    });
+});
+
+// Close all pins and exit
+setTimeout(function() {
+    gpio.destroy(function() {
+	console.log('All pins closed... bye');
+	process.exit();
+    });
+}, 10000);
